2d6cd220a0f2f200754a40c624ebf3268edb1421
lysepten <tlstlsrbrb11@gmail.com>
2024-05-01 14:59:20 +0900

    이미지 크롤링 후 DB에 이미지URL 넣는 작업 완료

--- src/main/java/com/example/demo/crawling/clothesSnapshotCrawling.java
+++ src/main/java/com/example/demo/crawling/clothesSnapshotCrawling.java
@@ -1,6 +1,10 @@
 package com.example.demo.crawling;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -9,15 +13,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-record RegionInfoTips(String information, String voltage, String language, String climate, String timeDifference,
-		String rate, String tips, int regionId) {
+record snapshotUrl(String url, int fashionStyleId) {
 
 }
 
 public class clothesSnapshotCrawling {
 
 	private WebDriver driver;
-	private String url;
+	private String snapUrl;
 
 	public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
 	public static String WEB_DRIVER_PATH = "C:/work/chromedriver.exe";
@@ -29,9 +32,50 @@ public class clothesSnapshotCrawling {
 		options.setCapability("ignoreProtectedModeSettings", true);
 		driver = new ChromeDriver(options);
 
-		url = "https://www.musinsa.com/search/musinsa/coordi?q=%EA%B7%B8%EB%9F%B0%EC%A7%80&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
-		driver.get(url);
+		// 무신사 스냅샷 그런지 스타일 이미지
+		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EA%B7%B8%EB%9F%B0%EC%A7%80&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+		driver.get(snapUrl);
+		
+		// 클래식룩 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%ED%81%B4%EB%9E%98%EC%8B%9D%EB%A3%A9&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
+		// 스트릿 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EC%8A%A4%ED%8A%B8%EB%A6%BF&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
+		// 캐주얼 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EC%BA%90%EC%A3%BC%EC%96%BC&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
+		// 히피 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%ED%9E%88%ED%94%BC&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
 
+		// 스포티 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EC%8A%A4%ED%8F%AC%ED%8B%B0&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+
+		// 보헤미안 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EB%B3%B4%ED%97%A4%EB%AF%B8%EC%95%88&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+
+		// 바이커 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EB%B0%94%EC%9D%B4%EC%BB%A4%EB%A3%A9&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
+		// 펑크 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%ED%8E%91%ED%81%AC&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
+		// 리조트 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EB%A6%AC%EC%A1%B0%ED%8A%B8%EB%A3%A9&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
+		// 빈티지 스타일 이미지
+//		snapUrl = "https://www.musinsa.com/search/musinsa/coordi?q=%EB%B9%88%ED%8B%B0%EC%A7%80%EB%A3%A9&list_kind=small&sortCode=term_date&sub_sort=&page=1&display_cnt=0&saleGoods=&includeSoldOut=&setupGoods=&popular=&category1DepthCode=&category2DepthCodes=&category3DepthCodes=&selectedFilters=&category1DepthName=&category2DepthName=&brandIds=&price=&colorCodes=&contentType=&styleTypes=&includeKeywords=&excludeKeywords=&originalYn=N&tags=&campaignId=&serviceType=&eventType=&type=&season=&measure=&openFilterLayout=N&selectedOrderMeasure=&shoeSizeOption=&d_cat_cd=&attribute=&plusDeliveryYn=";
+//		driver.get(snapUrl);
+		
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -44,18 +88,34 @@ public class clothesSnapshotCrawling {
 		// 리스트 이미지 전체
 		List<WebElement> images = driver.findElements(By.cssSelector("img"));
 
+		String url = "jdbc:mysql://127.0.0.1:3306/Spring_AM_01?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull";
+	    Connection conn = DriverManager.getConnection(url, "root", "");
+	    String insertQuery = "INSERT INTO snapshotUrl (url, fashionStyleId) VALUES (?, ?)";
+	    PreparedStatement pstmt = conn.prepareStatement(insertQuery);
+		
 		// 이미지 주소가 null일 경우 건너뛴다.
 		for (WebElement image : images) {
 		    if(image.getAttribute("data-original") != null) {
+		    	
 		    	String imageUrl = image.getAttribute("data-original");
+		    	int fashionStyleId = 6;
+		    	
+		    	pstmt.setString(1, imageUrl);
+		    	pstmt.setInt(2, fashionStyleId);
+		    	pstmt.executeUpdate();
+		    	
 		    	System.out.println("이미지 주소: " + imageUrl);
 		    }
 		}
+		
+//		List<snapshotUrl> snapshotUrlList = new ArrayList<>();
 
-
+		System.out.println("DB 전송 완료");
+		
+		conn.close();
 		driver.close();
 	}
-
+	
 	public static void main(String[] args) {
 		clothesSnapshotCrawling crawler = new clothesSnapshotCrawling();
 	    try {
